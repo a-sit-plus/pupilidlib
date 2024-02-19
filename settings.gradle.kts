d056b8d07053410ccfea6643d076244bca8c2c59
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-02-19 17:45:31 +0100

    update all dependencies incl vclib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,28 +25,3 @@ includeBuild("vclib") {
 
 rootProject.name = "pupilidlib"
 include(":pupilidlib")
-
-startParameter.excludedTaskNames+="transformNativeMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+="transformAppleMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+="transformIosMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+="transformNativeTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+="transformAppleTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+="transformIosTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformNativeMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformAppleMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformIosMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformNativeTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformAppleTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib:transformIosTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformNativeMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformAppleMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformIosMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformNativeTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformAppleTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-aries:transformIosTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformNativeMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformAppleMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformIosMainCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformNativeTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformAppleTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
-startParameter.excludedTaskNames+=":vclib:vclib-openid:transformIosTestCInteropDependenciesMetadataForIde" //disable broken import on non-macOS
